06d1a97ec2055fb9381bc427e71e0d5a05e32781
olufikayoolowoyo <olufikayoolowoyo@gmail.com>
2023-12-06 15:25:10 -0500

    Moduarize Routes-1

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
-const path = require("path");
+const path = require('path');
+const routes = require('./routes');
 
-app.set("view engine", "ejs"); // Notify Express to use Ejs as views engine
-app.set("views", path.join(__dirname, "./views")); // Specify path to Views
+app.set('view engine', 'ejs'); // Notify Express to use Ejs as views engine
+app.set('views', path.join(__dirname, './views')); // Specify path to Views
 
 // Configure loading of static files
-app.use(express.static(path.join(__dirname, "./static")));
+app.use(express.static(path.join(__dirname, './static')));
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.render("pages/index", { pageTitle: " Website MEN" });
-  // res.sendFile(path.join(__dirname, "./static/index.html"));
-});
+app.use('/', routes());
 
 app.listen(PORT, () => {
   console.log(`Server is listening to PORT:${PORT}`);
