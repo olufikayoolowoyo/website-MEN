483ebad14ee4a27883ebe4292117ec1f550099ff
olufikayoolowoyo <olufikayoolowoyo@gmail.com>
2023-12-06 15:53:38 -0500

    SubRoute-1

--- routes/product.js
+++ routes/product.js
@@ -2,9 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = () => {
-  router.get('/product', (req, res) => {
-    res.send('Hello from Products');
-  });
+  router
+    .get('/', (req, res) => {
+      res.send('Getting all Products....');
+    })
+    .get('/:id', (req, res) => {
+      res.send(`Getting Product with Id ${req.params.id}....`);
+    })
+    .put('/:id', (req, res) => {
+      res.send(`Update Product with Id ${req.params.id}....`);
+    })
+    .delete('/:id', (req, res) => {
+      res.send(`Deleting Product with Id ${req.params.id}....`);
+    })
+    .post('/', (req, res) => {
+      res.send('Hello from Post Product....');
+    });
 
   return router;
 };
--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = () => {
-  router.get('/user', (req, res) => {
-    res.send('Hello from Users....');
-  });
+  router
+    .get('/', (req, res) => {
+      res.send('Getting all Users....');
+    })
+    .get('/:id', (req, res) => {
+      res.send(`Getting Users with Id ${req.params.id}....`);
+    })
+    .put('/:id', (req, res) => {
+      res.send(`Update Users with Id ${req.params.id}....`);
+    })
+    .delete('/:id', (req, res) => {
+      res.send(`Deleting Users with Id ${req.params.id}....`);
+    })
+    .post('/', (req, res) => {
+      res.send('Hello from Post Users....');
+    });
 
   return router;
 };
